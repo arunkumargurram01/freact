c9a5d818d4701e88e9ae10615806d7e33326d60e
arunkumargurram01 <144266255+arunkumargurram01@users.noreply.github.com>
2024-05-17 14:43:57 +0530

    Update LoginPage.js

--- src/ServerCompo/LoginPage.js
+++ src/ServerCompo/LoginPage.js
@@ -7,6 +7,7 @@ import Cookies from "js-cookie";
 const LoginPage = () => {
 
     const navigate = useNavigate()
+    const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
     const[userCredentials, setUserCredentials] = useState({
         username : '',
         password : '',
@@ -26,7 +27,7 @@ const LoginPage = () => {
     const localcookie = async () => {
         console.log(userCredentials)
         try {
-            const response = await fetch('https://fserver-1.onrender.com/login', {
+            const response = await fetch(`${API_BASE_URL}/login`, {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
